6e68535013cf5e14a66b4e67c74e15c169b96bfd
Andrew Cave <andrew.j.cave@gmail.com>
2025-06-30 13:18:12 +1000

    Update list-pipeline-jobs-for-jenkinsfile-name.groovy
    added job location path to filter the set of jobs that are searched

--- admin-scripts/list-pipeline-jobs-for-jenkinsfile-name.groovy
+++ admin-scripts/list-pipeline-jobs-for-jenkinsfile-name.groovy
@@ -3,21 +3,24 @@ println("==========================================================")
 
 count = 0
 def details = false
+def jobLocationPattern = "DEV-Builds"
 def search = 'contains'
-def pattern = 'Jenkinsfile-Dev-Build'
-//def pattern = 'Jenkinsfile-Dev-Trial'
+def jenkinsFilePattern = 'Jenkinsfile-Dev-Build'
+//def jenkinsFilePattern = 'Jenkinsfile-Dev-Trial'
 
 // List all pipeline jobs
-  Jenkins.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob).each{ job ->
+Jenkins.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob).each{ job ->
     
-    def match = job.configFile.file.find { it."$search"(pattern) } != null 
-    if (match || job.name."$search"(pattern)) {
-        println "<a href=\"${job.absoluteUrl}configure\">${job.name}</a> matches"
-        ++count
+	if (job.fullName.contains(jobLocationPattern)) {
+    	def match = job.configFile.file.find { it."$search"(jenkinsFilePattern) } != null 
+       	if (match || job.name."$search"(jenkinsFilePattern)) {
+        	println "<a href=\"${job.absoluteUrl}configure\">${job.name}</a> matches"
+          	++count
 
-        if (details.toBoolean()) {
-          job.configFile.file.findAll { it."$search"(pattern) }.each { println '    ' + it.trim() }
-        }
-      }
-  }
-  return
+          	if (details.toBoolean()) {
+            	job.configFile.file.findAll { it."$search"(pattern) }.each { println '    ' + it.trim() }
+          	}
+       }
+    }
+}
+return
